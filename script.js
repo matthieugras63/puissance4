eb0cfae99465ab991971f1e77d2f7d639bae11f9
matthieugras63 <matthieu.gras.63@gmail.com>
2019-08-29 15:39:08 +0200

    function to count on diagonal added

--- script.js
+++ script.js
@@ -57,8 +57,8 @@ for (var i = 0; i < cells.length; i++) {
     var whichId=this.id; /* récupération de l'id de la cellule sur laquelle on click */
     var x = whichId.charAt(0); /* récupération de la première valeur. Format x:y, on récupèrera l'index 0, à savoir x ici */
     var y = whichId.charAt(2); /* idem avec l'index 2 */
-    // console.log("ligne: " +x);
-    // console.log("colonne: " +y);
+    console.log("ligne: " +x);
+    console.log("colonne: " +y);
     var placesRestantes = 0 /* initialisation du nombre de places dispo restantes dans la colonne */
     for (var i = 1; i < 7; i++) {
       var column = document.getElementById(i+':'+y); /* cette boucle vérifiera les contenus des cellules dans la colonne y */
@@ -86,6 +86,12 @@ for (var i = 0; i < cells.length; i++) {
     var countLeft=0;
     var sum = 0;
     var sumBottom =0;
+    var sumDiagTopRight=0;
+    var sumDiagTopLeft=0;
+    var countTopRight =0;
+    var countTopLeft =0;
+    var countBotLeft=0;
+    var countBotRight=0;
     var currentId=cellToFill.id;
     var z = parseInt(currentId.charAt(0),10); /* récupération des chiffres de l'id, et conversion de string à int */
     var t = parseInt(currentId.charAt(2),10);
@@ -96,26 +102,26 @@ for (var i = 0; i < cells.length; i++) {
       var childRight = contains.children; /* récupération des éléments enfants */
       if(childRight[0]!= undefined){ /* vérification que le premier enfant n'est pas inexistant */
         // console.log(childRight[0].style.background);
-        if (couleur ==="red"){
-          if (childRight[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
-            countRight++;
-          } else {
-            break;
-          }
+      if (couleur ==="red"){
+        if (childRight[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
+          countRight++;
         } else {
-          if (childRight[0].style.background==="red"){
-            countRight++;
-          } else {
-            break;
-          }
+          break;
+        }
+      } else {
+        if (childRight[0].style.background==="red"){
+          countRight++;
+        } else {
+          break;
         }
       }
     }
-    /* Ici, on aura la fonction qui calculera le nombre de voisins de la même couleur par la gauche */
-    for (var i=t-1; i>0; i--){
-      var contains = document.getElementById(z+':'+i);
-      var childLeft = contains.children;
-      if(childLeft[0]!= undefined){
+  }
+  /* Ici, on aura la fonction qui calculera le nombre de voisins de la même couleur par la gauche */
+  for (var i=t-1; i>0; i--){
+    var contains = document.getElementById(z+':'+i);
+    var childLeft = contains.children;
+    if(childLeft[0]!= undefined){
         // console.log(childLeft[0].style.background);
         if (couleur ==="red"){
           if (childLeft[0].style.background==="yellow"){
@@ -153,18 +159,122 @@ for (var i = 0; i < cells.length; i++) {
         }
       }
     }
-    sum = countLeft+countRight; /* ajout des 2 sommes des pions alignés sur la droite et la gauche */
-    if (sum === 3 || sumBottom ===3) { /* somme de 3 car le pion posé est compté */
-      if (couleur ==="red") {
-        alert("Le joueur 1 remporte la manche");
-        newGame();
+    /* Calcule le nombe de voisins par la diagonale haute droite */
+
+    for (var i=z-1, j= t+1; i>0, j < 8; i--, j++){
+      var contains = document.getElementById(i+':'+j);
+      console.log(contains);
+      if(contains != null){
+        var childTopRight = contains.children; /* récupération des éléments enfants */
+        if(childTopRight[0]!= undefined){ /* vérification que le premier enfant n'est pas inexistant */
+        // console.log(childRight[0].style.background);
+      if (couleur ==="red"){
+        if (childTopRight[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
+          countTopRight++;
+        } else {
+          break;
+        }
       } else {
-        alert("Le joueur 2 remporte la manche");
-        newGame();
+        if (childTopRight[0].style.background==="red"){
+          countTopRight++;
+        } else {
+          break;
+        }
       }
     }
   }
+}
+/* Calcule le nombre de voisins par la diagonale haute gauche */
 
+for (var i=z-1, j= t-1; i>0, j > 0; i--, j--){
+  var contains = document.getElementById(i+':'+j);
+  console.log(contains);
+  if (contains != null){
+    var childTopLeft = contains.children; /* récupération des éléments enfants */
+    if(childTopLeft[0]!= undefined){ /* vérification que le premier enfant n'est pas inexistant */
+        // console.log(childRight[0].style.background);
+      if (couleur ==="red"){
+        if (childTopLeft[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
+          countTopLeft++;
+        } else {
+          break;
+        }
+      } else {
+        if (childTopLeft[0].style.background==="red"){
+          countTopLeft++;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+}
+
+/* Calcule le nombre de voisins par la diagonale basse gauche */
+
+for (var i=z+1, j= t-1; i<7, j >0; i++, j--){
+  var contains = document.getElementById(i+':'+j);
+  console.log(contains);
+  if (contains != null){
+    var childBotLeft = contains.children; /* récupération des éléments enfants */
+    if(childBotLeft[0]!= undefined){ /* vérification que le premier enfant n'est pas inexistant */
+        // console.log(childRight[0].style.background);
+      if (couleur ==="red"){
+        if (childBotLeft[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
+          countBotLeft++;
+        } else {
+          break;
+        }
+      } else {
+        if (childBotLeft[0].style.background==="red"){
+          countBotLeft++;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+}
+/* Calcule le nombre de voisins par la diagonale basse droite */
+
+for (var i=z+1, j= t+1; i<8, j < 8; i++, j++){
+  var contains = document.getElementById(i+':'+j);
+  console.log(contains);
+  if (contains != null){
+    var childBotRight = contains.children; /* récupération des éléments enfants */
+    if(childBotRight[0]!= undefined){ /* vérification que le premier enfant n'est pas inexistant */
+        // console.log(childRight[0].style.background);
+      if (couleur ==="red"){
+        if (childBotRight[0].style.background==="yellow"){ /* comparaison de la couleur des background des cercles pour permettre le calcul du nombre de pions alignés */
+          countBotRight++;
+        } else {
+          break;
+        }
+      } else {
+        if (childBotRight[0].style.background==="red"){
+          countBotRight++;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+sum = countLeft+countRight; /* ajout des 2 sommes des pions alignés sur la droite et la gauche */
+sumDiagTopRight = countTopRight+countBotLeft;
+sumDiagTopLeft = countTopLeft + countBotRight;
+if (sum === 3 || sumBottom ===3 || sumDiagTopRight ===3 || sumDiagTopLeft ===3) { /* somme de 3 car le pion posé est compté */
+  if (couleur ==="red") {
+    alert("Le joueur 1 remporte la manche");
+    newGame();
+  } else {
+    alert("Le joueur 2 remporte la manche");
+    newGame();
+  }
+}
+}
 }
 
 
@@ -184,7 +294,6 @@ var note = document.createElement('div');
 document.getElementById('playerTurn').appendChild(note);
 note.style.paddingTop="30px";
 note.style.fontSize="20px";
-note.innerHTML = "Attention. La vérification des éléments alignés en diagonale n'est pas fonctionnelle (pour le moment). Soyez donc prudents, et vérifiez par vous-même";
 function whoIsPlaying(){
   if (couleur == "yellow"){
     playing.innerHTML = "C'est au joueur 1 de jouer";
